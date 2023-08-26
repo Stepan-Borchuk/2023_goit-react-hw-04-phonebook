c50c15d8eb11b9f8364e2ee01cfa42c22ab9623d
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2023-08-26 22:35:31 +0300

    2

---
+++ src/App/App.jsx
@@ -0,0 +1,20 @@
+
+import { Box } from './Box';
+
+
+export const App = () => {
+  return (
+    <Box
+      color="text"
+      p={4}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      fontFamily="body"
+      fontWeight="body"
+      fontSize="s"
+      flexDirection="column">
+    
+    </Box>
+  );
+};
---
+++ src/App/Box.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import {
+  color,
+  space,
+  layout,
+  shadow,
+  typography,
+  flexbox,
+  border
+} from 'styled-system';
+
+export const Box = styled('div')(
+  color,
+  space,
+  layout,
+  shadow,
+  typography,
+  flexbox, border
+);
