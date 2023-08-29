9b8741759b76b9a03a087a0d8080af4fd1f0fc6e
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2023-08-29 21:43:49 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,21 @@ class App extends Component {
     ],
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   submitForm = (values, { resetForm }) => {
     const oldContact = this.state.contacts.find(
       person => person.name.toLowerCase() === values.name.toLowerCase()
